35cd82c1d1ee7636d41dd300d808d3dfc1f7737e
Natasha Setiadi <natset456@gmail.com>
2020-10-28 08:20:44 -0400

    fixed talon numbers

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -17,10 +17,10 @@ package frc.robot;
 public class Constants {
 
 
-	public static final int LEFT_FRONT_DRIVE_TALON_PORT  = 6;
-	public static final int LEFT_BACK_DRIVE_TALON_PORT   = 7;
-	public static final int RIGHT_FRONT_DRIVE_TALON_PORT = 1;
-	public static final int RIGHT_BACK_DRIVE_TALON_PORT  = 3;
+	public static final int LEFT_FRONT_DRIVE_TALON_PORT  = 3;
+	public static final int LEFT_BACK_DRIVE_TALON_PORT   = 6;
+	public static final int RIGHT_FRONT_DRIVE_TALON_PORT = 11;
+	public static final int RIGHT_BACK_DRIVE_TALON_PORT  = 2;
 	// public static final int XBOX_CONTROLLER_1_PORT = 0;
     // public static final int XBOX_CONTROLLER_2_PORT = 1;
 
